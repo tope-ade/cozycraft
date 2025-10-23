4e3c732fc689cee2d175e04a7ba6eb424d53677c
topesam <topesam99@gmail.com>
2025-10-23 08:56:54 +0100

    before step six

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,17 @@
 
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+ name : {type : String, required : true},
+
+ category : {type : String, required : true},
+
+ price : {type : Number, required : true},
+
+ description : {type : String, required : true},
+
+ imageUrl : {type : String}
+
+}, {timestamps : true});
+
+module.export = mongoose.model('Product', productSchema);
--- server.js
+++ server.js
@@ -1,1 +1,34 @@
 
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
+
+//initialize express
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//call connectDB function
+connectDB();
+
+//API routes
+app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
+
+//serve frontend static files from /public
+app.use(express.static(path.join(__dirname, 'public')));
+
+//fallback for SPA if needed (optional)
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 4040;
+app.listen(PORT, () => console.log(`server running on http://localhost:${PORT}`));
